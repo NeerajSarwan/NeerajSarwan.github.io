10dad88b9eb0aa7ea71cd6ae0dceb89ef851c98a
Neeraj Singh Sarwan <neerajsinghsarwan@Neerajs-MacBook-Air.local>
2022-08-28 19:16:09 +0530

    change find function

--- script.py
+++ script.py
@@ -3,6 +3,22 @@ import requests
 import random
 import argparse
 import string as st
+
+def Find_(string):
+    s = string[:]
+    urls = []
+    while(len(s)>0):
+        http_index = s.find("http")
+        if http_index!=-1:
+            sub_s = s[http_index:]
+            quotes_index = sub_s.find('"')
+            bracket_index = sub_s.find(')')
+            closing_index = min(quotes_index, bracket_index)
+            urls.append(s[http_index:http_index+closing_index])
+            s = s[http_index+closing_index+1:]
+        else:
+            break
+    return urls
   
 def Find(string):
     """
@@ -10,7 +26,7 @@ def Find(string):
     """
     # findall() has been used 
     # with valid conditions for urls in string
-    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»]))"
     url = re.findall(regex,string)      
     return [x[0] for x in url]
 
@@ -33,14 +49,18 @@ def download_img(image_url):
     """
     Downloads image from a valid image url and returns the saved image name
     """
-    img_data = requests.get(image_url).content
-    img_name = generate_random_string()
-    full_img_name = "./files/posts/{}/{}".format(CLEANED_FILENAME, img_name)
-    with open(full_img_name, 'wb') as handler:
-        handler.write(img_data)
-    full_img_name = full_img_name.split("/")
-    full_img_name[0] = "https://neerajsarwan.github.io"
-    full_img_name = "/".join(full_img_name)
+    img_response = requests.get(image_url)
+    if img_response.status_code == 200:
+        img_data = img_response.content
+        img_name = generate_random_string()
+        full_img_name = "./files/posts/{}/{}".format(CLEANED_FILENAME, img_name)
+        with open(full_img_name, 'wb') as handler:
+            handler.write(img_data)
+        full_img_name = full_img_name.split("/")
+        full_img_name[0] = "https://neerajsarwan.github.io"
+        full_img_name = "/".join(full_img_name)
+    else:
+        print("Defective URL: {}".format(image_url))
     return full_img_name
 
 def url_processing(string):
@@ -50,7 +70,7 @@ def url_processing(string):
     3. downloads the images in the URL to a local location
     4. Replace the web url with the local url
     """
-    found_urls = Find(string)
+    found_urls = Find_(string)
     filtered_urls = filtering_function(found_urls)
     name_mapping = {}
     for URL in filtered_urls:
